075fc5aa4e159fe11889f586cdaef3d5ba7811fa
faryozbekTFK <t.faryozbek1311@gmail.com>
2021-08-10 17:10:08 +0500

    Loading Images

--- main.js
+++ main.js
@@ -1,60 +1,115 @@
-const BOX = document.getElementById("box");
-const LOADBTN = document.getElementById("loading");
-const REFBTN = document.getElementById("refresh");
-const LOADTEXT = document.getElementById("loadText");
-let IMGS = [],
+const MINCONT = document.querySelectorAll(".minCont");
+const BOX = document.querySelectorAll(".box");
+const LOADBTN = document.querySelectorAll(".loading");
+const REFBTN = document.querySelectorAll(".refresh");
+const LOADTEXT = document.querySelectorAll(".loadText");
+let boxId = 0,
+  IMGS = [],
   idImg = 0,
   time = 2000;
-function addImage(img) {
+
+function addImage(img, callBox) {
   console.log("Calling addImage Method");
   const imgElement = document.createElement("img");
   imgElement.src = img;
+  imgElement.classList.add = `imgs${boxId}`;
   imgElement.alt = "no-empty";
   IMGS.push(imgElement);
-  BOX.appendChild(imgElement);
+  if (IMGS.length % 4 === 0) {
+    console.log("IMGS: ", IMGS);
+    for (let i = IMGS.length - 4; i < IMGS.length; i++)
+      callBox.appendChild(IMGS[i]);
+  }
 }
 
-function loadImg(url, callback) {
+function loadImg(url, callback, callBox, texLoad) {
   console.log("image loading . . .");
-  LOADTEXT.innerText = "Loading...";
-
+  texLoad.innerText = "Loading...";
   setTimeout(() => {
     const image = new Image();
     image.onload = () => {
       console.log("image loaded . . .");
-      LOADTEXT.style.display = "none";
-      callback(image.src);
+      texLoad.style.display = "none";
+      callback(image.src, callBox);
     };
     image.src = url;
   }, time);
 }
 
-LOADBTN.addEventListener("click", () => {
-  loadImg(`https://picsum.photos/id/${idImg++}/200/200`, addImage);
-  loadImg(`https://picsum.photos/id/${idImg++}/200/200`, addImage);
-  loadImg(`https://picsum.photos/id/${idImg++}/200/200`, addImage);
-  loadImg(`https://picsum.photos/id/${idImg++}/200/200`, addImage);
-  LOADBTN.disabled = "false";
-  LOADBTN.style.cursor = "no-drop";
-  LOADBTN.style.background = "gray";
-  setTimeout(() => {
-    LOADBTN.style.display = "none";
-    REFBTN.style.display = "flex";
-  }, time + 500);
+LOADBTN.forEach((btnLoad, idx) => {
+  btnLoad.addEventListener("click", () => {
+    console.log("Loading...");
+    loadImg(
+      `https://picsum.photos/id/${idImg++}/200/200`,
+      addImage,
+      BOX[idx],
+      LOADTEXT[idx]
+    );
+    loadImg(
+      `https://picsum.photos/id/${idImg++}/200/200`,
+      addImage,
+      BOX[idx],
+      LOADTEXT[idx]
+    );
+    loadImg(
+      `https://picsum.photos/id/${idImg++}/200/200`,
+      addImage,
+      BOX[idx],
+      LOADTEXT[idx]
+    );
+    loadImg(
+      `https://picsum.photos/id/${idImg++}/200/200`,
+      addImage,
+      BOX[idx],
+      LOADTEXT[idx]
+    );
+    boxId++;
+    btnLoad.disabled = "false";
+    btnLoad.style.cursor = "no-drop";
+    btnLoad.style.background = "gray";
+    setTimeout(() => {
+      btnLoad.style.display = "none";
+      REFBTN[idx].style.display = "flex";
+      if (boxId < 3) MINCONT[boxId].style.display = "flex";
+    }, time + 500);
+  });
 });
 
-REFBTN.addEventListener("click", () => {
-  console.log("Refresh...");
-  REFBTN.style.background = "gray";
-  REFBTN.style.cursor = "no-drop";
-  for (let i = 0; i < IMGS.length; i++) IMGS[i].style.display = "none";
-  LOADTEXT.style.display = "flex";
-  loadImg(`https://picsum.photos/id/${idImg++}/200/200`, addImage);
-  loadImg(`https://picsum.photos/id/${idImg++}/200/200`, addImage);
-  loadImg(`https://picsum.photos/id/${idImg++}/200/200`, addImage);
-  loadImg(`https://picsum.photos/id/${idImg++}/200/200`, addImage);
-  setTimeout(() => {
-    REFBTN.style.background = "red";
-    REFBTN.style.cursor = "pointer";
-  }, time + 500);
+REFBTN.forEach((btnRef, idx) => {
+  btnRef.addEventListener("click", () => {
+    console.log("Refresh...");
+    btnRef.style.cursor = "no-drop";
+    btnRef.style.background = "gray";
+    LOADTEXT[idx].style.display = "flex";
+    loadImg(
+      `https://picsum.photos/id/${idImg++}/200/200`,
+      addImage,
+      BOX[idx],
+      LOADTEXT[idx]
+    );
+    loadImg(
+      `https://picsum.photos/id/${idImg++}/200/200`,
+      addImage,
+      BOX[idx],
+      LOADTEXT[idx]
+    );
+    loadImg(
+      `https://picsum.photos/id/${idImg++}/200/200`,
+      addImage,
+      BOX[idx],
+      LOADTEXT[idx]
+    );
+    loadImg(
+      `https://picsum.photos/id/${idImg++}/200/200`,
+      addImage,
+      BOX[idx],
+      LOADTEXT[idx]
+    );
+    btnRef.disabled = true;
+    setTimeout(() => {
+      btnRef.disabled = false;
+      btnRef.style.cursor = "pointer";
+      btnRef.style.background = "red";
+    }, time + 500);
+  });
 });
